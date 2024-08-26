9dda23146fbe6dff5ada9ab508bb29eabecf9951
gh242 <gh242@msn.com>
2024-08-26 13:48:04 +0800

    Day4-15-詳情頁--SKU組件熟悉使用

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DetailHot from "@/views/Detail/components/DetailHot.vue";
 import ImageView from '@/components/ImageView/index.vue'
+import XtxSku from '@/components/XtxSku/index.vue'
 import {getDetail} from '@/apis/detail'
 import {ref, onMounted} from 'vue'
 import {useRoute} from 'vue-router'
@@ -13,6 +14,11 @@ const getGoods = async() =>{
 }
 
 onMounted(() => getGoods() )
+
+//sku規格被操作時
+const skuChange = (sku) => {
+  console.log(sku)
+} 
 </script>
 
 <template>
@@ -101,7 +107,7 @@ onMounted(() => getGoods() )
                 </dl>
               </div>
               <!-- sku组件 -->
-
+              <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 數據组件 -->
 
               <!-- 按鈕组件 -->
