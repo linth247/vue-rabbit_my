11ecfa68bb2c54b892d151392c0fa368b62f546e
gh242 <gh242@msn.com>
2024-08-27 20:44:11 +0800

    Day6-11-結算-切換地址-地址激活交互實現

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -4,7 +4,7 @@ import { getCheckoutInfoAPI } from '@/apis/checkout'
 import { ref, onMounted } from 'vue'
 
 const checkInfo = ref({})  // 訂單對象
-const curAddress = ref({}) // 地址對象
+const curAddress = ref({}) // 默認地址對象
 const getCheckInfo = async() =>{
   const res = await getCheckoutInfoAPI()
   checkInfo.value = res.result
@@ -17,6 +17,18 @@ onMounted(() => getCheckInfo())
 
 // 控制彈框打開
 const showDialog = ref(false)
+
+// 切换收貨地址
+const activeAddress = ref({})
+const switchAddress = (item)=>{
+  activeAddress.value = item;
+}
+const confirm = () => {
+  curAddress.value = activeAddress.value
+  showDialog.value = false
+  activeAddress.value = {}
+}
+
 
 
 </script>
@@ -121,7 +133,7 @@ const showDialog = ref(false)
   <!-- 切換地址 -->
   <el-dialog v-model="showDialog" title="切換貨地址" width="30%" center>
     <div class="addressWrapper">
-    <div class="text item" v-for="item in checkInfo.userAddresses"  :key="item.id">
+    <div class="text item" :class="{ active:activeAddress.id === item.id }" @click="switchAddress(item)" v-for="item in checkInfo.userAddresses"  :key="item.id">
       <ul>
       <li><span>收<i />貨<i />人：</span>{{ item.receiver }} </li>
       <li><span>聯絡方式：</span>{{ item.contact }}</li>
@@ -132,7 +144,7 @@ const showDialog = ref(false)
   <template #footer>
     <span class="dialog-footer">
       <el-button>取消</el-button>
-      <el-button type="primary">確定</el-button>
+      <el-button type="primary" @click="confirm">確定</el-button>
     </span>
   </template>
 </el-dialog>
