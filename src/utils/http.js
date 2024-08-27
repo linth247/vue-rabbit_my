31293df6a912a6e3a743c3c5095e69fcb32a372a
gh242 <gh242@msn.com>
2024-08-27 10:47:44 +0800

    Day5-11-登入-Token失效401攔截處理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
  baseURL:'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -24,12 +25,20 @@ httpInstance.interceptors.request.use(config => {
   
 // axios响应式攔截器
 httpInstance.interceptors.response.use(res => res.data, e => {
-    // 統一錯誤提示
-    ElMessage({
-        type:'warning',
-        message: e.response.data.message
-    })
-    return Promise.reject(e)
+	const userStore = useUserStore()
+	// 統一錯誤提示
+	ElMessage({
+			type:'warning',
+			message: e.response.data.message
+	})
+	// 401token失效處理
+	// 1.清除本地用戶數據
+	// 2.跳轉到登入頁
+	if(e.response.status === 401){
+		userStore.clearUserInfo()
+		router.push('/login')
+	}
+	return Promise.reject(e)
 })
 
 export default httpInstance
